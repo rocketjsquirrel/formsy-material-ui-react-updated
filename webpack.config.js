68395d17bea9a612c838e89aeb1e437f7be584bf
Seth Soffer <ssoffer@byrdalley.com>
2020-03-02 11:33:08 -0500

    build output

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,71 @@
-const webpack = require('webpack');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const webpack           = require( 'webpack' );
+const HTMLWebpackPlugin = require( 'html-webpack-plugin' );
+const path              = require( 'path' );
 
 const config = {
-  entry: [path.resolve(__dirname, 'src', 'examples', 'index.js')],
-
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  devServer: {
-    port: 8080,
-    publicPath: '/',
-    contentBase: path.resolve(__dirname, 'public'),
-    historyApiFallback: true,
-    disableHostCheck: true,
-    hot: false,
-    open: process.env.BROWSER !== 'none',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js[x]?$/,
-        exclude: /(node_modules)/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react'],
-              plugins: [
-                [
-                  '@babel/plugin-transform-runtime',
-                  {
-                    regenerator: true,
-                  },
-                ],
-                '@babel/plugin-proposal-object-rest-spread',
-                '@babel/plugin-proposal-class-properties',
-                [
-                  '@babel/plugin-proposal-decorators',
-                  {
-                    decoratorsBeforeExport: true,
-                  },
-                ],
-              ],
-            },
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', { loader: 'css-loader', options: { importLoaders: 1 } }], //, 'postcss-loader',],
-      },
-    ],
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      minify: {
-        collapseWhitespace: false,
-      },
-      hash: true,
-      title: 'Formsy Material UI',
-      template: path.resolve(__dirname, 'src', 'examples', 'index.html'),
-    }),
-  ],
+	entry:     [ path.resolve( __dirname, 'src', 'examples', 'index.js' ), path.resolve( __dirname, 'src', 'forms', 'index.js' ) ],
+	output:    {
+		path:          path.resolve( __dirname, 'build' ),
+		filename:      'index.js'
+	},
+	resolve:   {
+		extensions: [ '.js', '.jsx' ],
+	},
+	devServer: {
+		port:               8080,
+		publicPath:         '/',
+		contentBase:        path.resolve( __dirname, 'public' ),
+		historyApiFallback: true,
+		disableHostCheck:   true,
+		hot:                false,
+		open:               process.env.BROWSER !== 'none',
+	},
+	module:    {
+		rules: [
+			{
+				test:    /\.js[x]?$/,
+				exclude: /(node_modules)/,
+				use:     [
+					{
+						loader:  'babel-loader',
+						options: {
+							presets: [ '@babel/preset-env', '@babel/preset-react' ],
+							plugins: [
+								[
+									'@babel/plugin-transform-runtime',
+									{
+										regenerator: true,
+									},
+								],
+								'@babel/plugin-proposal-object-rest-spread',
+								'@babel/plugin-proposal-class-properties',
+								[
+									'@babel/plugin-proposal-decorators',
+									{
+										decoratorsBeforeExport: true,
+									},
+								],
+							],
+						},
+					},
+				],
+			},
+			{
+				test: /\.css$/,
+				use:  [ 'style-loader', { loader: 'css-loader', options: { importLoaders: 1 } } ], //, 'postcss-loader',],
+			},
+		],
+	},
+	plugins:   [
+		new HTMLWebpackPlugin( {
+			minify:   {
+				collapseWhitespace: false,
+			},
+			hash:     true,
+			title:    'Formsy Material UI',
+			template: path.resolve( __dirname, 'src', 'examples', 'index.html' ),
+		} ),
+	],
 };
 
 module.exports = config;
